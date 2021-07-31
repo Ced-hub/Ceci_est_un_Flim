0901b82062737a92075b16030c67daa8d6715cf4
Ced-hub <cedric_benoit@hotmail.fr>
2021-07-31 17:14:55 +0200

    changement front

--- exoccp1p4/assets/js/main.js
+++ exoccp1p4/assets/js/main.js
@@ -28,15 +28,16 @@ apiKey = '5cef6346';
                                 success: function (dataBis) {
                                     console.log(dataBis);
                                     $('#displayFilm').append(`
-                                        <div class="card col-auto mt-5 me-5 ms-5 shadow" style="width: 30%;">
+                                        <div class="card col-auto mt-5 me-5 ms-5 mb-5 shadow" style="width: 35%; background-color: #212529;">
                                                 <img src="${dataBis.Poster}" class="card-img-top" alt="${dataBis.Title}">
-                                            <div class="card-body">
+                                            <div class="card-body" style="background-color: #212529;">
                                                 <h5 class="card-title" style="color: red;"><u>${dataBis.Title}</u></h5>
-                                                <p class="card-text">
-                                                    <strong>Date de sortie:</strong> ${dataBis.Released}<br>
-                                                    <strong>Acteurs:</strong> ${dataBis.Actors}<br>Durée: ${dataBis.Runtime}<br>
-                                                    <strong>Genre:</strong> ${dataBis.Genre}<br>
-                                                    <strong>Synopsis:</strong> ${dataBis.Plot}
+                                                <p class="card-text" style="color: white;">
+                                                    <strong>Date de sortie :</strong> ${dataBis.Released}<br>
+                                                    <strong>Acteurs :</strong> ${dataBis.Actors}<br>
+                                                    <strong>Durée : </strong>${dataBis.Runtime}<br>
+                                                    <strong>Genre :</strong> ${dataBis.Genre}<br>
+                                                    <strong>Synopsis :</strong> ${dataBis.Plot}
                                                 </p>
                                             </div>
                                         </div>`
@@ -45,15 +46,6 @@ apiKey = '5cef6346';
                                 }
                             })
                     }
-                },
-
-                error: function (erreur) {
-                    console.log('Requête impossible : ')
-                    console.log(erreur)
-                },
-
-                complete: function (data) {
-                    console.log('Terminé')
-                }
+                },          
             })
         }
